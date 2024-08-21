0a709dda773dfa56d9f706fae0a96fcaee12778e
Siddhesh-yerramneni <siddhesh.yerramneni@gmail.com>
2024-08-20 23:29:34 -0400

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+     <h1 className='text-3xl text-yellow-300'>Currently under development - Minions are have been notified to get into the work!!</h1>
+    </div>
+  )
+}
